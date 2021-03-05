e5ed26adc1e10649c587c2765c16ff1b100e4473
Bruno Attina <bruattina@gmail.com>
2021-03-05 00:18:37 -0300

    Rota para Index criada e Layout Blade da View Index criada

---
+++ app/Http/Controllers/VendasController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+class VendasController extends Controller
+{
+	public function index(){
+		return view('vendas.index');
+	}
+}
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'VendasController@index')->name('listar_series');
