b1ad2b9e5c674fb3c9480ca5bba8f6cfd22eb9a5
Bruno Attina <bruattina@gmail.com>
2021-03-24 23:30:22 -0300

    Acertando a pagina inicial

--- resources/views/vendas/index.blade.php
+++ resources/views/vendas/index.blade.php
@@ -44,7 +44,7 @@
 	<ul class="list-group">
 
 	@foreach($vendas as $venda)
-		<li class="list-group-item d-flex justify-content-between align-items-center">{{ $venda->id }} <?php echo $venda->created_at.$venda->valor;?> 
+		<li class="list-group-item d-flex justify-content-between align-items-center">{{ $venda->id }} <?php echo $venda->created_at.$venda->valor.$venda->fkCliente;?> 
 			<form method="post" action="/series/remover/{{$venda->id}}" 
 				onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($venda->nome) }}?')"> <!--addslashes função que faz ignorar " ' " no nome-->
 				@csrf
@@ -59,38 +59,46 @@
 
 
 	<div id="accordion">
+	@foreach($clientes as $cliente)
 	  <div class="card">
 	    <div class="card-header" id="headingOne">
 	      <h5 class="mb-0">
-	        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-	          Pessoa 1
+	        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+	        	{{$cliente->nome}}
 	        </button>
 	      </h5>
 	    </div>
 
-	    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-	      <div class="card-body">
-	        <table class="table">
-				<thead>
-					<tr>
-						<th scope="col">#</th>
-						<th scope="col"></th>
-						<th scope="col">Last</th>
-						<th scope="col">Handle</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-				</tbody>
-			</table>
-	      </div>
+	    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">      
+			<div class="card-body">
+				<table class="table">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Nome</th>
+							<th scope="col">Produto</th>
+							<th scope="col">Valor</th>
+							<th scope="col">?</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($vendas as $venda)
+							@if($venda->fkCliente == $cliente->id)
+							<tr>
+								<th scope="row">1</th>
+								<td>{{$cliente->nome}}</td>
+								<td>{{$venda->produto}}</td>
+								<td>{{$venda->valor}}</td>
+								<td></td>
+							</tr>
+							@endif
+						@endforeach
+					</tbody>
+				</table>
+			</div>
 	    </div>
 	  </div>
+	@endforeach  
 	  <div class="card">
 	    <div class="card-header" id="headingTwo">
 	      <h5 class="mb-0">
